7f869f161e3c4ba8756ac063ac3c0c55399e7780
ben4you2 <bento297@hotmail.com>
2025-03-14 19:41:42 -0400

    update

--- AppFor/app.js
+++ AppFor/app.js
@@ -221,3 +221,19 @@ document.getElementById("voterLookupForm").addEventListener("submit", function(e
         `;
   }
 });
+
+/*Alert for users */
+
+/*Alert*/
+async function presentAlert(header, subHeader, message) {
+  const alert = document.createElement('ion-alert');
+  alert.header = 'Ben4you Technology';
+  alert.subHeader = 'The program is working with Ben';
+  alert.message = 'A message should be a short, complete and full sentence.';
+  alert.background = 'darkgrey';
+  alert.buttons = ['Ok']['Cancel'];
+  
+  
+  document.body.appendChild(alert);
+  await alert.present();
+}
---
+++ files/script.js
@@ -0,0 +1,18 @@
+// script.js
+/*
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active'); // Toggle the 'active' class
+});*/
+
+
+// script.js
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
